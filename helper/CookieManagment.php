fa826f07553d0c83da29ba2f7aec1557c30a8c4a
teachwiki <35622257+teachwiki@users.noreply.github.com>
2021-01-05 09:03:44 -0800

    Update CookieManagment.php

--- helper/CookieManagment.php
+++ helper/CookieManagment.php
@@ -14,7 +14,7 @@ class CookieManagment {
      * Path in which the cookie will be available
      * @var string
      */
-    const PATH		= 'ibook/';
+    const PATH		= '/';
 
     /**
      * Domain that the cookie is available
